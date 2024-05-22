0c44a5e37b9d893ae6c03a658073d39518d3d885
Amdadul Haque <122418243+Amdau1@users.noreply.github.com>
2024-05-22 09:40:18 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,11 +8,11 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    let shadowEl = this.attachShadow({mode:'open'});
+    let shadow = this.attachShadow({mode:'open'});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     let markup = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    let styles = document.createElement('style');
+    let style = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     styles.textContent = `
     * {
@@ -92,9 +92,9 @@ class RecipeCard extends HTMLElement {
     }
   `;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    shadowEl.append(markup);
-    shadowEl.append(styles);
-    this.shadowEl = shadowEl;
+    shadow.append(markup);
+    shadow.append(style);
+    this.shadow = shadow;
   }
 
   /**
@@ -123,12 +123,12 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    let articleEl = this.shadowRoot.querySelector("article");
+    let article = this.shadowRoot.querySelector("article");
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
-    articleEl.innerHTML =  
+    article.innerHTML =  
   `<img src="${data["imgSrc"]}"
       alt="${data["imgAlt"]}">
     <p class="title">
